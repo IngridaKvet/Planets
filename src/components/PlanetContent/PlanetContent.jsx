11552e1261fa9fb27f3b1329fa0a4f8537d9d677
ingrida <ingridakvetkovskyte@gmail.com>
2025-07-01 16:13:06 +0300

    Add main text component

--- src/components/PlanetContent/PlanetContent.jsx
+++ src/components/PlanetContent/PlanetContent.jsx
@@ -1,5 +1,24 @@
-const PlanetContent = () => {
-    return ( <p>planet content</p> );
-}
- 
+import styles from "./planetContent.module.scss";
+import SourceIcon from "../../assets/icon-source.svg";
+
+const PlanetContent = ({ planet }) => {
+  return (
+    <section className={styles.mainContent}>
+      <h1 className="extraLargeHeading">{planet.name}</h1>
+      <p className="bodyText">{planet.overview.content}</p>
+      <p className={`bodyText ${styles.linkText}`}>
+        Source: {"  "}
+        <a
+          href={planet.overview.source}
+          target="_blank"
+          className={styles.link}
+        >
+          Wikipedia
+        </a>{" "}
+        <img src={SourceIcon} alt="Source icon" />{" "}
+      </p>
+    </section>
+  );
+};
+
 export default PlanetContent;
--- src/components/PlanetPage/PlanetPage.jsx
+++ src/components/PlanetPage/PlanetPage.jsx
@@ -3,15 +3,16 @@ import PlanetStats from "../PlanetStats/PlanetStats";
 import PlanetImage from "../PlanetImage/PlanetImage";
 import PlanetContent from "../PlanetContent/PlanetContent";
 
-
 const PlanetPage = ({ planet }) => {
   return (
     <section className={styles.page}>
-        <div className={styles.contentWrapper}>
-        <PlanetImage planet={planet} tab={"geology"}/>
-        <PlanetContent planet={planet}/>
-</div>
-      
+      <div className={styles.contentWrapper}>
+        <PlanetImage planet={planet} tab={"geology"} />
+        <div>
+          <PlanetContent planet={planet} />
+          <div className={styles.tabs}></div>
+        </div>
+      </div>
 
       <PlanetStats planet={planet} />
     </section>
